564c4f3a1afd4f48ef75c5be2ebdf0ccaa988fab
Jayden Chan <jaydencn7@gmail.com>
2019-07-27 16:13:21 -0700

    JC: Add Dvorak + remove weird LED stuff

--- keyboards/dztech/dz60rgb/keymaps/jayden-chan/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/jayden-chan/keymap.c
@@ -11,39 +11,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_ESC,          KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,   KC_QUOT,           KC_ENT, \
                 KC_LSFT,         KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,   KC_UP, \
                 KC_LCTL,         KC_LGUI,  KC_LALT,                KC_SPC,                         MO(1),    KC_LEFT,   KC_DOWN,           KC_RIGHT),
-		[_LAYER1] = LAYOUT_ANSI( /* FN */
-				KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
-			    KC_TRNS,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_TRNS,  KC_MPLY,  KC_MPRV,  KC_MNXT,  RESET  ,\
-			    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_HOME,  KC_END,             KC_TRNS,\
-				KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MUTE,  KC_TRNS,  KC_PGDN,  KC_PSCR,                      KC_TRNS, \
-				KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_MPLY,                                KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS),
-		[_LAYER2] = LAYOUT_ANSI( /* FN2 */
+		[_LAYER1] = LAYOUT_ANSI( /* FN Layer */
 				KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
-				KC_TRNS,  RGB_TOG,  KC_TRNS,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  RGB_MOD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET  ,\
-				KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SPI,  RGB_SPD,  KC_TRNS,  KC_TRNS,            KC_TRNS,\
-				KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, \
-				KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS),
-		[_LAYER3] = LAYOUT_ANSI( /* FN3 */
-				KC_GESC,       KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,\
-			    KC_TAB,        KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,  KC_RBRC, KC_BSLASH,\
-				CTL_T(KC_CAPS),KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,           KC_ENT, \
-			    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,  \
-				KC_LCTL,       KC_LALT,  KC_LGUI,                KC_SPC,                           KC_TRNS,MO(4),   KC_RALT,           KC_RCTL),
-		[_LAYER4] = LAYOUT_ANSI( /* FN4 */
-				KC_GESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
-			    KC_TRNS,  RGB_TOG,  KC_TRNS,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  RGB_MOD,  KC_PSCR,  KC_SLCK,  KC_PAUS,  RESET  ,\
-			    KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SPI,  RGB_SPD,  KC_HOME,  KC_PGUP,            KC_TRNS,\
-				KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_END,   KC_PGDOWN,KC_MNXT, \
-				KC_TRNS,  KC_TRNS,  KC_TRNS,                      TO(0),                                  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS),
+			    KC_TRNS,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_TRNS,  KC_MPLY,  KC_MPRV,  KC_MNXT,  RESET  ,\
+			    KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_HOME,  KC_END,             KC_TRNS,\
+				TO(3),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MUTE,  KC_TRNS,  KC_PGDN,  KC_PSCR,                      KC_TRNS, \
+				KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_MPLY,                                KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS),
+		[_LAYER2] = LAYOUT_ANSI( /* Dvorak Layout */
+                KC_GRV,          KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,      KC_MINS,  KC_EQL,  KC_BSPC,\
+                KC_TAB,          KC_QUOT,  KC_COMM,  KC_DOT,KC_P,  KC_Y,  KC_F,    KC_G,  KC_C,    KC_R,     KC_L,      KC_SLSH,  KC_UQL,  KC_BSLASH,\
+                KC_ESC,          KC_A,     KC_O,     KC_E,  KC_U,  KC_I,  KC_D,    KC_H,  KC_T,    KC_N,     KC_S,      KC_MINS,           KC_ENT, \
+                KC_LSFT,         KC_SCLN,  KC_Q,     KC_J,  KC_K,  KC_X,  KC_B,    KC_M,  KC_W,    KC_V,     KC_Z,      KC_UP, \
+                KC_LCTL,         KC_LGUI,  KC_LALT,                KC_SPC,                         MO(3),    KC_LEFT,   KC_DOWN,           KC_RIGHT),
+		[_LAYER3] = LAYOUT_ANSI( /* FN Layer (Identical to FN layer 1 other than left shift)*/
+				KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
+			    KC_TRNS,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_TRNS,  KC_MPLY,  KC_MPRV,  KC_MNXT,  RESET  ,\
+			    KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_HOME,  KC_END,             KC_TRNS,\
+				TO(0),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BRID,  KC_BRIU,  KC_MUTE,  KC_TRNS,  KC_PGDN,  KC_PSCR,                      KC_TRNS, \
+				KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_MPLY,                                KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS),
 		};
 
-void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue, bool default_layer) {
-  for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) {
-        rgb_matrix_set_color( i, red, green, blue );
-    }
-  }
-}
+void rgb_matrix_layer_helper (uint8_t red, uint8_t green, uint8_t blue, bool default_layer) {}
 
 void rgb_matrix_indicators_user(void) {
 	  uint8_t this_led = host_keyboard_leds();
