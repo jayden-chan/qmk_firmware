b06a69ed2a0c2b8b59a5db6774ebb2e8760cbda3
Jayden Chan <jaydencn7@gmail.com>
2019-11-05 18:23:35 -0800

    JC: Add GTA layer

--- keyboards/dztech/dz60rgb/keymaps/jayden-chan/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/jayden-chan/keymap.c
@@ -2,8 +2,10 @@
 #define DEFAULT_LAYER 0
 #define DVORAK_LAYER 1
 #define CSGO_LAYER 2
-#define FN_LAYER 3
-#define FN_CSGO_LAYER 4
+#define GTA_LAYER 3
+#define FN_LAYER 4
+#define FN_CSGO_LAYER 5
+#define FN_GTA_LAYER 6
 
 #define QQ_DELAY 6
 
@@ -38,6 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,       KC_SLSH,  KC_UP, \
                 KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                            MO(FN_CSGO_LAYER), KC_LEFT,  KC_DOWN,            KC_RIGHT),
 
+        [GTA_LAYER] = LAYOUT_ANSI(
+                KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,         KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,\
+                KC_TAB,   CU_QQ,    KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_P7,        KC_P8,    KC_P9,    KC_RBRC,  KC_BSLASH,\
+                KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_P4,        KC_P5,    KC_P6,              KC_ENT, \
+                KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_P1,    KC_P2,        KC_P3,    KC_UP, \
+                KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                            MO(FN_GTA_LAYER),  KC_LEFT,  KC_DOWN,            KC_RIGHT),
+
         [FN_LAYER] = LAYOUT_ANSI(
                 KC_F13,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,        KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
                 CU_TEST,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  CU_PASS,      KC_MPLY,  KC_MPRV,  KC_MNXT,  RESET  ,\
@@ -46,6 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_MPLY,                                KC_TRNS,      KC_TRNS,  KC_TRNS,            KC_TRNS),
 
         [FN_CSGO_LAYER] = LAYOUT_ANSI(
+                KC_F13,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,        KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
+                KC_TRNS,  KC_Q,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_INS,       KC_P7,    KC_P8,    KC_P9,    KC_TRNS,\
+                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P4,        KC_P5,    KC_P6,              DF(GTA_LAYER),\
+                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P1,    KC_P2,        KC_P3,    KC_P0, \
+                KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                KC_TRNS,      KC_PSLS,  KC_PAST,            KC_PMNS),
+
+        [FN_GTA_LAYER] = LAYOUT_ANSI(
                 KC_F13,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,        KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
                 KC_TRNS,  KC_Q,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_INS,       KC_P7,    KC_P8,    KC_P9,    KC_TRNS,\
                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_P4,        KC_P5,    KC_P6,              DF(DEFAULT_LAYER),\
